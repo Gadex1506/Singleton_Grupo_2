3704a5705e9a8b6246c7e8403bc27df48424a6ce
David <jonathan.torresmg1506@gmail.com>
2025-08-07 22:22:41 -0500

    Funcion Update Contraseña Implementada
    Se implemento la funcion Update en el proyecto para que el usuario pueda actualizar unicamente la contraseña de un usuario previamente registrado.
    
    Se valida que el usuario se encuentre registrado.
    
    La nueva contraseña debe ser diferente a la contraseña actual que tiene el usuario

--- src/main/java/view/VistaUsuario.java
+++ src/main/java/view/VistaUsuario.java
@@ -10,7 +10,9 @@ import model.Usuario;
 
 public class VistaUsuario {
 
+    // CREATE
     public void solicitarDatos() {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("**** BIENVENIDO AL SISTEMA SINGLETON ****");
         System.out.println("Por favor ingresar el usuario:");
@@ -30,4 +32,55 @@ public class VistaUsuario {
         }
     }
 
+    // UPDATE
+    public void actualizarUsuario() {
+        
+        ControladorUsuario usuarioControlador = new ControladorUsuario();
+        Scanner sc = new Scanner(System.in);
+        String usuario = "";
+        String nuevaContrasenia = "";
+        boolean diferenteContrasenia;
+
+        System.out.println("Ingresa tu nombre de usuario");
+        usuario = sc.nextLine(); // Nombre del usuario registrado en la base de datos
+
+        // Validar si el usuario existe y guardar la posicion donde se encuentra el usuario
+        int posUsuario = usuarioControlador.existeUsuario(usuario);
+
+        if (posUsuario != -1) {
+
+            // El usuario debera ingresar una contrasenia diferente a la actual
+            // De lo contrario se le seguira solicitando que ingrese una nueva contrasenia
+            do {
+
+                System.out.println("Ingresa la nueva contraseña para: " + usuario);
+
+                // Guardar nueva contrasenia
+                nuevaContrasenia = sc.nextLine();
+
+                // Validar que la nueva contrasenia sea diferente a la actual
+                diferenteContrasenia = usuarioControlador.mismaContrasenia(posUsuario, nuevaContrasenia);
+
+                if (diferenteContrasenia) {
+                    // Objeto usuario auxiliar con el mismo usuario original pero con la nueva contrasenia
+                    Usuario usuarioModelo = new Usuario(usuario, nuevaContrasenia);
+
+                    // Actualizar la contrasenia del usuario en la base de datos
+                    boolean actualizado = usuarioControlador.actualizarUsuario(posUsuario, usuarioModelo);
+
+                    if (actualizado) {
+                        System.out.println("¡Usuario Actualizado Existosamente!");
+                    } else {
+                        System.out.println("Algo salio mal al actualizar el usuario. Vuelve a intentarlo.");
+                    }
+                } else {
+                    System.out.println("La nueva contraseña debe ser diferente a la contraseña actual que tienes.");
+                }
+
+            } while (!diferenteContrasenia);
+
+        } else {
+            System.out.println("El usuario no se encuentra registrado en la base de datos.");
+        }
+    }
 }
